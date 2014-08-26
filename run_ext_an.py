aca497eb06e292042d70ff0e20b2fc318384c29a
Erwin Haasnoot <e.haasnoot.3@umail.leidenuniv.nl>
2014-08-26 16:21:23 +0000

    Full run

--- run_ext_an.py
+++ run_ext_an.py
@@ -6,34 +6,34 @@ import os
 import utils.funcs as funcs
 
 dataFile = 'data_by_cookie_slim.json'
-outputFolder = 'analysis1'
+outputFolder = 'analysis2'
 outputSuffix = '_run1'
-iterations = 2
+iterations = 100
 epochmult = 40
 #sys.argv = [dataFile, outputFolder, outputSuffix, iterations, epochmult]
 
 funcs.ensurePath(outputFolder)
 
-# #Run perceptron analysis
-# import ee_binaryclass.runPerceptron as runPerceptron
-# runPerceptron.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#Run perceptron analysis
+import ee_binaryclass.runPerceptron as runPerceptron
+runPerceptron.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
-# #Run MLP analysis 
-# import ee_binaryclass.runMLP as runMLP
-# runMLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#Run MLP analysis 
+import ee_binaryclass.runMLP as runMLP
+runMLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
-# #Run local/global quit analysis with MLP
-# import ee_binaryclass.lgquit_MLP as lgquit_MLP
-# lgquit_MLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#Run local/global quit analysis with MLP
+import ee_binaryclass.lgquit_MLP as lgquit_MLP
+lgquit_MLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
-# funcs.ensurePath(outputFolder + '/logRegress')
-# funcs.ensurePath(outputFolder + '/varMean')
+funcs.ensurePath(outputFolder + '/logRegress')
+funcs.ensurePath(outputFolder + '/varMean')
 
-# import ee_boot.runLogRegress as runLogRegress
-# runLogRegress.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+import ee_boot.runLogRegress as runLogRegress
+runLogRegress.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
-# import ee_boot.runVarMean as runVarMean
-# runVarMean.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+import ee_boot.runVarMean as runVarMean
+runVarMean.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
 #Create graphs
 import graphs.ee_bootGraphs as ee_bootGraphs
