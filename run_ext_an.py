1d8f83dcdf69255f61bfc9677ab82e61da1b5ee3
Erwin <erwinhaasnoot@gmail.com>
2014-08-27 12:55:31 +0100

    Slightly changed graph

--- run_ext_an.py
+++ run_ext_an.py
@@ -15,25 +15,25 @@ epochmult = 40
 funcs.ensurePath(outputFolder)
 
 #Run perceptron analysis
-import ee_binaryclass.runPerceptron as runPerceptron
-runPerceptron.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#import ee_binaryclass.runPerceptron as runPerceptron
+#runPerceptron.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
 #Run MLP analysis 
-import ee_binaryclass.runMLP as runMLP
-runMLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#import ee_binaryclass.runMLP as runMLP
+#runMLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
 #Run local/global quit analysis with MLP
-import ee_binaryclass.lgquit_MLP as lgquit_MLP
-lgquit_MLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#import ee_binaryclass.lgquit_MLP as lgquit_MLP
+#lgquit_MLP.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
 funcs.ensurePath(outputFolder + '/logRegress')
 funcs.ensurePath(outputFolder + '/varMean')
 
-import ee_boot.runLogRegress as runLogRegress
-runLogRegress.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#import ee_boot.runLogRegress as runLogRegress
+#runLogRegress.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
-import ee_boot.runVarMean as runVarMean
-runVarMean.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
+#import ee_boot.runVarMean as runVarMean
+#runVarMean.main(dataFile, outputFolder, outputSuffix, iterations, epochmult)
 
 #Create graphs
 import graphs.ee_bootGraphs as ee_bootGraphs
