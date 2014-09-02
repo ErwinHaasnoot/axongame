2a2dfacbd6a58c074fe834cd0bd909d84caaad07
Erwin <erwinhaasnoot@gmail.com>
2014-09-02 15:45:49 +0100

    Added new Analyses

---
+++ ee_boot/runVarMax.py
@@ -0,0 +1,33 @@
+#Import modules
+import pickle
+import numpy as np
+import utils.funcs as funcs
+
+#Import project files
+import utils.bootstrap as bs
+import utils.observed as os
+
+def main(datafile = '../data_by_cookie_slim.json', outputFolder = '.', outputSuffix = '', iterations = 10, epochmult = 4):
+    #Load data
+    print 'Running analysis of variance vs mean correlation'
+    outextension = '/varMax'
+    outputFolder = outputFolder + outextension
+    funcs.ensurePath(outputFolder)
+    data = funcs.loadData(datafile)
+    
+    processY = lambda x,x_plays: np.amax(x,axis=0)
+    print 'iterations: {}\noutput folder: {}\n'.format(iterations,outputFolder)
+    
+    print "OBSERVED DATA"
+    os.runObs(data, outputFolder, processY = processY)
+    
+    print "BOOTSTRAP"
+    bs.runBoot(data, iterations, outputFolder, processY = processY)
+    
+    print
+    print
+    
+    return [outextension]
+
+if __name__ == "__main__":
+    main(datafile, outputFolder, outputSuffix, iterations, epochmult)
